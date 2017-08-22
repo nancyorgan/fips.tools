57acadae39e78d23bfec0997a368b966ee01023c
nancyorgan <nancyorgan@gmail.com>
2017-08-22 10:55:33 -0700

    First

---
+++ R/FIPS_converter.R
@@ -0,0 +1,33 @@
+#' Convert coordinates to FIPS
+#'
+#' Convert geocoded coordinates to a FIPS code. Can be used for a data set or a single data point.
+#' @param id Id to be preserved in the output
+#' @param lat Latiutude
+#' @param long Longitude
+#' @param state_data Shapefiles for the state
+#' @export
+#' @examples
+#' Mayo = grab_shapefiles(c("MN", "WI"))
+#' FIPS_converter(id = c("House 1", "House 2", "House 3"),
+#'                lat = c(44.079216, 45.8283963, 46.1252037),
+#'                long = c(-92.66854, -95.4019813, -95.4569129),
+#'                state_data = Mayo)
+#'
+#'             id         FIPS
+#' 1 Some house 1 271090019002
+#' 2 Some house 2 270419509001
+#' 3 Some house 3 271119615001
+
+FIPS_converter = function(id, lat, long, state_data){
+  dat = data.frame(id, lat, long)
+  rownames(dat) = dat$id
+  coordinates(dat) <- ~ long + lat
+  proj4string(dat) <- proj4string(state_data)
+  final = over(dat, state_data)
+  final$id = row.names(final)
+  row.names(final) = c(1:length(id))
+  final = subset(final, select = c("id", "BKGPIDFP00"))
+  names(final) = c("id", "FIPS")
+  return(final)
+}
+
---
+++ R/grab_shapefiles.R
@@ -0,0 +1,30 @@
+#' Download TIGER Shapefiles
+#'
+#' Downlaod shapefiles for a state based on the state abbreviation.
+#' @param state_abbrv The standard two-letter state abbreviation
+#' @export
+#' @examples
+#' MN = grab_shapefiles("MN")
+#' Mayo_shapefiles = grab_shapefiles(c("MN", "WI"))
+#' plot(Mayo_shapefiles) # Be careful, this is slow!
+
+
+grab_shapefiles = function(state_abbrv){
+  state_data = list()
+  state_number = NULL
+  for(i in 1:length(unique(state_abbrv))){
+    state_number[i] = fipsdf$code[fipsdf$abbrv == state_abbrv[i]]
+    temp.dir = tempdir()
+    temp.file = tempfile()
+    download.file(url = paste0("https://www2.census.gov/geo/tiger/TIGER2010/BG/2000/tl_2010_",
+                               state_number[i],"_bg00.zip"), destfile = temp.file)
+    data = unzip(zipfile = temp.file, exdir = temp.dir)
+    state_data[[i]] = readOGR(grep(".shp$", data, value = TRUE))
+    unlink(temp.dir)
+    unlink(temp.file)
+  }
+  final = do.call(rbind,c(state_data,list(makeUniqueIDs=TRUE)))
+  return(final)
+}
+
+# this = grab_shapefiles(c("MN", "WI"))
